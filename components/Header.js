e37be30ed90a7013d52d32d8235a2902038dcdcb
karimfawaz <38626543+karimfawaz@users.noreply.github.com>
2022-03-27 04:10:51 +0100

    Header init

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import styles from '../styles/Header.module.css'
+
+
+const Header = () => {
+    return (
+        <div className={styles.headerContainer}>
+            <div className={styles.logoName}>
+                <img className={styles.logo} src='/logo_transparent_no_text.png' />
+                <svg className={styles.nameSvg}>
+                    <text className={styles.svgText} x="50%" y="50%" fill="" textAnchor='middle'>
+                        Thresh
+                    </text>
+                </svg>
+            </div>
+        </div>
+    )
+}
+
+export default Header
